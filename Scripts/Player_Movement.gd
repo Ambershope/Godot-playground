2b9a461c7a05f5980f2f29bee18d7b14d3c99e09
Ambershope <148982579+Ambershope@users.noreply.github.com>
2024-08-06 18:38:54 +0200

    movement + look

--- Scripts/Player_Movement.gd
+++ Scripts/Player_Movement.gd
@@ -1,10 +1,12 @@
 extends CharacterBody2D
 
 
-const maxSpeed = 70
+const speed = 70
 const acceleraton = 10
 
 var input: Vector2
+
+
 
 func calcMovementVector():
 	input.x = Input.get_action_strength("Right") - Input.get_action_strength("Left")
@@ -14,9 +16,24 @@ func calcMovementVector():
 func processMovementVector(delta):
 	var playerVector = calcMovementVector()
 	
-	velocity = lerp(velocity, playerVector * maxSpeed, delta * acceleraton)
+	velocity = lerp(velocity, playerVector * speed, delta * acceleraton)
 
 	move_and_slide()
 
+func cameraLooksPlayer():
+	var player = global_transform.origin
+	Camera2D.global_transform.origin = player
+
+func playerLooksAtCursor():
+	var mousePos: Vector2 = get_global_mouse_position()
+	var playerPos: Vector2 = global_transform.origin
+	look_at(mousePos)
+	
+	
+	
+
 func _physics_process(delta):
 	processMovementVector(delta)
+	
+	playerLooksAtCursor()
+	
